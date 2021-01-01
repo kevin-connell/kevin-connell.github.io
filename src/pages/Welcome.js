c4bec07dc1df638efd9f690a5c4517a45929040b
FileOtter <kevin.t.connell@gmail.com>
2021-01-01 16:17:05 -0500

    Skills section added

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -2,6 +2,7 @@ import React from "react";
 import WelcomeMessage from "../compnents/WelcomeMessage";
 import ProjectList from "../compnents/ProjectList";
 import AboutMe from "../compnents/AboutMe";
+import Skills from "../compnents/Skills";
 
 
 
@@ -10,6 +11,7 @@ function Welcome() {
         <>
         <WelcomeMessage />
         <ProjectList />
+        <Skills />
         <AboutMe />
         </>
     );
